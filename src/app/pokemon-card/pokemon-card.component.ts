d779b3f3def48d8882aaff97fc5076d9dbddafb6
Lucas Jose da Costa <lucjoc.1@hotmail.com>
2023-06-27 16:00:04 -0300

    Add pokemon list

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/models/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-card.component.scss']
 })
 export class PokemonCardComponent {
+  @Input()
+  public pokemon!: Pokemon;
 
+  leadingZero(str: string| number, size = 3): string{
+    let s = String(str);
+    while(s.length < (size || 2)){
+      s = '0' + s;
+    }
+    return s;
+  }
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pokemon } from 'src/models/Pokemon';
+import { Type } from 'src/models/Type';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -6,5 +8,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-list.component.scss']
 })
 export class PokemonListComponent {
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      type: [
+        Type.Grass,
+        Type.Poison
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      number: 2,
+      name: 'Ivysaur',
+      type: [
+        Type.Grass,
+        Type.Poison
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+      number: 3,
+      name: 'Venusaur',
+      type: [
+        Type.Grass,
+        Type.Poison
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
+      number: 4,
+      name: 'Charmander',
+      type: [
+        Type.Fire
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png',
+      number: 5,
+      name: 'Charmeleon',
+      type: [
+        Type.Fire
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png',
+      number: 6,
+      name: 'Charizard',
+      type: [
+        Type.Fire,
+        Type.Flying
+      ]
+    }
+  ];
+
 
 }
